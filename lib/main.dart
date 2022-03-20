9e05ebf56128413fd9ee2b4f0def653f2fcea954
Adam Katav <adamkatav@gmail.com>
2022-03-21 01:48:49 +0200

    Moved to inheretance

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,14 @@ import 'package:flame_forge2d/forge2d_game.dart';
 import 'package:flutter/widgets.dart';
 import 'package:test_flame/boundaries.dart';
 
+Vector2 vec2Median(List<Vector2> vecs) {
+  var sum = Vector2(0, 0);
+  for (final v in vecs) {
+    sum += v;
+  }
+  return sum / vecs.length.toDouble();
+}
+
 void main() {
   final game = MyGame();
   runApp(GameWidget(game: game));
@@ -26,16 +34,24 @@ class MyGame extends Forge2DGame with MultiTouchDragDetector, HasTappables {
     boundaries.forEach(add);
 
     groundBody = world.createBody(BodyDef());
-
-    add(Ball(screenToWorld(camera.viewport.effectiveSize / 2), 5));
-    add(Ball(screenToWorld(camera.viewport.effectiveSize / 2), 5));
+    final center = screenToWorld(camera.viewport.effectiveSize / 2);
+    var ball = Ball(center, 1, bodyType: BodyType.dynamic);
+    add(ball);
+    final poly = Polygon([
+      center + Vector2(0, 0),
+      center + Vector2(0, 5),
+      center + Vector2(5, 0),
+      //center + Vector2(5, 5)
+    ]);
+    add(poly);
+    grabbedBody = poly;
   }
 
   @override
   bool onDragUpdate(int pointerId, DragUpdateInfo details) {
     final mouseJointDef = MouseJointDef()
       ..maxForce = 3000 * grabbedBody.body.mass * 10 //Not neccerly needed
-      ..dampingRatio = 0.1
+      ..dampingRatio = 1
       ..frequencyHz = 5
       ..target.setFrom(grabbedBody.body.position)
       ..collideConnected = false //Maybe set to true
@@ -59,16 +75,18 @@ class MyGame extends Forge2DGame with MultiTouchDragDetector, HasTappables {
   }
 }
 
-class Ball extends BodyComponent with Tappable {
+abstract class TappableBodyComponent extends BodyComponent with Tappable {
   final Vector2 position;
-  final double radius;
   final BodyType bodyType;
-  Ball(this.position, this.radius, {this.bodyType = BodyType.dynamic});
+  TappableBodyComponent(this.position, {this.bodyType = BodyType.dynamic});
 
   @override
-  Body createBody() {
-    final shape = CircleShape();
-    shape.radius = radius;
+  bool onTapDown(_) {
+    MyGame.grabbedBody = this;
+    return false;
+  }
+
+  Body tappableBCreateBody(Shape shape) {
     final fixtureDef = FixtureDef(shape)
       ..restitution = 0.8
       ..density = 1.0
@@ -83,10 +101,30 @@ class Ball extends BodyComponent with Tappable {
 
     return world.createBody(bodyDef)..createFixture(fixtureDef);
   }
+}
+
+class Ball extends TappableBodyComponent {
+  final double radius;
+
+  Ball(Vector2 position, this.radius, {BodyType bodyType = BodyType.dynamic})
+      : super(position, bodyType: bodyType);
 
   @override
-  bool onTapDown(_) {
-    MyGame.grabbedBody = this;
-    return false;
+  Body createBody() {
+    final shape = CircleShape();
+    shape.radius = radius;
+    return tappableBCreateBody(shape);
+  }
+}
+
+class Polygon extends TappableBodyComponent {
+  final List<Vector2> vertecies;
+
+  Polygon(this.vertecies) : super(vec2Median(vertecies));
+
+  @override
+  Body createBody() {
+    final shape = PolygonShape()..set(vertecies);
+    return tappableBCreateBody(shape);
   }
 }
